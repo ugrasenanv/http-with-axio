e3f1aa0da4143d829443a4698610e3054df3177d
ugrasenanv <ugrasenanv@gmail.com>
2022-02-08 23:20:10 +0530

    1 file modified

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,54 +8,58 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts : [],
-        selectedPostId: null,
-        error:false
-    }
-    componentDidMount () {
-        axios.get("/posts").then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post=>{
-                    return{
-                        ...post,
-                        author:'Max'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-            }).catch(error=>{
-                this.setState({error:true})
-            })
-    };
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  };
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Max',
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => {
+        this.setState({ error: true });
+      });
+  }
 
-    postSelectHandler = (id) =>{
-        this.setState({selectedPostId:id})
-    }
+  postSelectHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
 
-    render () {
-        let posts = <p style={{textAlign:'center'}}>Something went wrong</p>
-        if(!this.state.error)
-            posts = this.state.posts.map(post => {
-                return <Post 
-                            key={post.id} 
-                            author={post.author} 
-                            title={post.title} 
-                            clicked={() => this.postSelectHandler(post.id)} />
-            })
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>;
+    if (!this.state.error)
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            key={post.id}
+            author={post.author}
+            title={post.title}
+            clicked={() => this.postSelectHandler(post.id)}
+          />
         );
-    }
+      });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        {/* <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section> */}
+        {/* <section>
+                    <NewPost />
+                </section> */}
+      </div>
+    );
+  }
 }
 
 export default Blog;
